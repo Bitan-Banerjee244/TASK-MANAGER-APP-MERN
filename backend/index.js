5eda14eef159c1bb9ba7f4ab0bfa9489ae841321
Bitan Banerjee <bbitan244@gmail.com>
2025-06-15 14:45:09 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ dotenv.config()
 const app = express()
 
 app.use(cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: [process.env.FRONTEND_URL,"https://task-manager-app-mern-1.onrender.com"],
     credentials: true
 }))
 
